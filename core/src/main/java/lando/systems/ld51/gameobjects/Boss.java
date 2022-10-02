5fcf3d0d059354330700ecff76468340c0ccf46e
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2022-10-01 23:51:58 -0500

    Switch boss animation to updated art

--- core/src/main/java/lando/systems/ld51/gameobjects/Boss.java
+++ core/src/main/java/lando/systems/ld51/gameobjects/Boss.java
@@ -3,8 +3,10 @@ package lando.systems.ld51.gameobjects;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import lando.systems.ld51.screens.GameScreen;
 
 public class Boss {
@@ -18,7 +20,7 @@ public class Boss {
     private float accum;
 
     private final GameScreen screen;
-    private final Animation<TextureRegion> animation;
+    private final Animation<TextureAtlas.AtlasRegion> animation;
     private TextureRegion keyframe;
     private float stateTime;
 
@@ -26,9 +28,8 @@ public class Boss {
         this.screen = screen;
         this.position = new Vector2(screen.arena.bounds.x + screen.arena.bounds.width/2f,
                           screen.arena.bounds.y + screen.arena.bounds.height/2f);
-        TextureRegion frame = screen.assets.atlas.findRegion("characters/wizard-glass-joe");
-        this.animation = new Animation<>(0.1f, frame);
-        this.animation.setPlayMode(Animation.PlayMode.LOOP);
+        Array<TextureAtlas.AtlasRegion> frames = screen.assets.atlas.findRegions("characters/boss-idle");
+        this.animation = new Animation<>(0.1f, frames, Animation.PlayMode.LOOP);
         this.keyframe = animation.getKeyFrame(0);
         this.stateTime = 0f;
     }

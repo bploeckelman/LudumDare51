261a002d8b52024a48078c7e85712c8b020f80b4
Doug Graham <dsgraham@wisc.edu>
2022-10-03 16:31:18 -0500

    really... dont show the boss arrow if they are on screen

--- core/src/main/java/lando/systems/ld51/gameobjects/BossArrow.java
+++ core/src/main/java/lando/systems/ld51/gameobjects/BossArrow.java
@@ -1,7 +1,9 @@
 package lando.systems.ld51.gameobjects;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import lando.systems.ld51.screens.GameScreen;
 
@@ -13,21 +15,33 @@ public class BossArrow {
     private float accum;
     Vector2 position;
     Vector2 direction;
+    Rectangle cameraBounds;
+
 
     public BossArrow(GameScreen screen){
         this.gameScreen = screen;
         this.position = new Vector2();
         this.direction = new Vector2();
+        this.cameraBounds = new Rectangle(0,0, 100, 100);
+
     }
 
     public void update(float dt) {
         accum += dt;
         direction.set(gameScreen.boss.position).sub(gameScreen.player.position).nor();
         position.set(gameScreen.windowCamera.viewportWidth/2f, gameScreen.windowCamera.viewportHeight/2f).mulAdd(direction, 180f + MathUtils.sin(accum*4f) * 40);
+        OrthographicCamera worldCamera = gameScreen.worldCamera;
+        cameraBounds.set(worldCamera.position.x - (worldCamera.viewportWidth/2 * worldCamera.zoom),
+                worldCamera.position.y - (worldCamera.viewportHeight/2 * worldCamera.zoom),
+                worldCamera.viewportWidth * worldCamera.zoom,
+                worldCamera.viewportHeight * worldCamera.zoom);
     }
 
     public void render(SpriteBatch batch) {
-        if (gameScreen.player.isWizard()) {
+        Vector2 bossPos = gameScreen.boss.position;
+
+        boolean bossOnScreen = cameraBounds.contains(bossPos);
+        if (gameScreen.player.isWizard() && !bossOnScreen) {
             batch.draw(gameScreen.assets.arrow, position.x - SIZE / 2f, position.y - SIZE / 2f, SIZE / 2f, SIZE / 2f, SIZE, SIZE, 1f, 1f, direction.angleDeg());
         }
     }
